620685a2d12f42e23632f6f236cc7fbdd1d0e0f6
Chamodya Sewwandi Gamage <Chamodyasewwandi99@gmail.com>
2024-07-06 11:19:27 +0530

    create react project

---
+++ Backend/BookStoreManagement/src/main/java/com/example/BookStoreManagement/BookStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookStoreManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreManagementApplication.class, args);
+	}
+
+}
---
+++ Backend/BookStoreManagement/src/test/java/com/example/BookStoreManagement/BookStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookStoreManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
