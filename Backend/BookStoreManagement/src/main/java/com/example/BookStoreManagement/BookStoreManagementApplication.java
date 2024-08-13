8270e5e89b233b3c1bc910980717c9c495b87343
Chamodya Sewwandi Gamage <Chamodyasewwandi99@gmail.com>
2024-08-13 22:01:52 +0530

    add Project

--- Backend/BookStoreManagement/src/main/java/com/example/BookStoreManagement/BookStoreManagementApplication.java
+++ Backend/BookStoreManagement/src/main/java/com/example/BookStoreManagement/BookStoreManagementApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookStoreManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookStoreManagementApplication.class, args);
+
+	SpringApplication.run(BookStoreManagementApplication.class, args);
 	}
 
 }
