6b5d38d0ab33aac11200a3e08a0be9305aac9354
Sanali Losathi <kklosathi19@gmail.com>
2024-08-15 11:37:10 +0530

    Created Category Entity

--- Backend/BookStoreManagement/src/main/java/com/example/BookStoreManagement/BookStoreManagementApplication.java
+++ Backend/BookStoreManagement/src/main/java/com/example/BookStoreManagement/BookStoreManagementApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookStoreManagementApplication {
 
 	public static void main(String[] args) {
-
-	SpringApplication.run(BookStoreManagementApplication.class, args);
+	 SpringApplication.run(BookStoreManagementApplication.class, args);
 	}
-
 }
---
+++ Backend/BookStoreManagement/src/main/java/com/example/BookStoreManagement/dto/CatrgoryDTO.java
@@ -0,0 +1,16 @@
+package com.example.BookStoreManagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class CatrgoryDTO {
+    private int categoryID;
+    private String categoryname;
+
+
+}
